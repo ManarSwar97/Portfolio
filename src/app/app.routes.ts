175d0769ab543b6daf93a6211db38a51906d885e
ManarSwar97 <manarswar97@gmail.com>
2025-08-24 12:40:52 +0300

    add experience section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { Welcome } from './welcome/welcome';
 import { About } from './about/about';
 import { NgModule } from '@angular/core';
+import { Experience } from './experience/experience';
 //adding routes for navbar to navigate between pages
 export const routes: Routes = [
     {path: '', redirectTo: 'welcome', pathMatch: 'full'},
     {path: 'welcome', component: Welcome},
     {path: 'about', component: About},
+    {path: 'experience', component: Experience},
+
     { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 @NgModule({
